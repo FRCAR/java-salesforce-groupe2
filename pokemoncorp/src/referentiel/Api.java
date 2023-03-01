f15d6b91902a66af93a3b6ab3abb485ae31e0a20
Khalil ALAOUI <alaouielomarikhalil@gmail.com>
2023-03-01 19:31:30 +0100

    Completing api code

--- pokemoncorp/src/referentiel/Api.java
+++ pokemoncorp/src/referentiel/Api.java
@@ -2,5 +2,29 @@ package pokemoncorp.src.referentiel;
 import java.util.HashMap;
 
 public class Api {
-    private Pokemon <Integer, String> mesPokemons = new HashMap<>();
+    private HashMap <Integer , Pokemon> mesPokemons = new HashMap<>(){{
+    put(1, new Racaillou());
+    put(2, new Roucoul());
+    put(3, new Carapuce());
+    put(4, new Salameche());
+}};
+
+    Api (){
+    
+    }
+
+    public HashMap <Integer, Pokemon> getAllPokemon (){
+        return mesPokemons;
+    }
+    
+    public Pokemon getPokemonByID(int id){
+        return mesPokemons.get((Integer) id);
+        
+    }
+
+    public void updatePokemon (Pokemon pokemon , int addedExperience){
+        pokemon.setExperience(pokemon.getExperience() + addedExperience);  
+        mesPokemons.replace(pokemon.getId(), pokemon);
+    }
+    
 }
