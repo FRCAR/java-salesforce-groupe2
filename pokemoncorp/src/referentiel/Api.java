53dd9794040fdc6e004e7d0c08927a0b776c3197
Khalil ALAOUI <alaouielomarikhalil@gmail.com>
2023-03-02 09:22:14 +0100

    Api indentation & comments

--- pokemoncorp/src/referentiel/Api.java
+++ pokemoncorp/src/referentiel/Api.java
@@ -1,30 +1,44 @@
 package pokemoncorp.src.referentiel;
+
 import java.util.HashMap;
 
 public class Api {
-    private HashMap <Integer , Pokemon> mesPokemons = new HashMap<>(){{
-    put(1, new Racaillou());
-    put(2, new Roucoul());
-    put(3, new Carapuce());
-    put(4, new Salameche());
-}};
+    /**
+     * this class acts as an external data source, it provides functions to retrieve
+     * :
+     * - all pokemons
+     * - One pokemon by providing its Id
+     * This api provides the possibilty to update a pokemon (add experience)
+     */
+    private HashMap<Integer, Pokemon> mesPokemons = new HashMap<>() {
+        {
+            put(1, new Racaillou());
+            put(2, new Roucoul());
+            put(3, new Carapuce());
+            put(4, new Salameche());
+        }
+    };
 
-    Api (){
-    
+    // Api Constructor with no parametrs
+    Api() {
+
     }
 
-    public HashMap <Integer, Pokemon> getAllPokemon (){
+    // Retrieving all pokemons
+    public HashMap<Integer, Pokemon> getAllPokemon() {
         return mesPokemons;
     }
-    
-    public Pokemon getPokemonByID(int id){
+
+    // Retrieving one pokemon by Id
+    public Pokemon getPokemonByID(int id) {
         return mesPokemons.get((Integer) id);
-        
+
     }
 
-    public void updatePokemon (Pokemon pokemon , int addedExperience){
-        pokemon.setExperience(pokemon.getExperience() + addedExperience);  
+    // Adding experience to a specific Pokemon
+    public void updatePokemon(Pokemon pokemon, int addedExperience) {
+        pokemon.setExperience(pokemon.getExperience() + addedExperience);
         mesPokemons.replace(pokemon.getId(), pokemon);
     }
-    
+
 }
