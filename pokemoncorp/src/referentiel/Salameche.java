40d7b8d547ae8208db1a5304674d4f5f72c620b0
Affani Sanae <affani.sanae@gmail.com>
2023-03-02 14:04:04 +0100

    ajout_attribut_nomClass_Pokemon

--- pokemoncorp/src/referentiel/Salameche.java
+++ pokemoncorp/src/referentiel/Salameche.java
@@ -3,6 +3,6 @@ package pokemoncorp.src.referentiel;
 public class Salameche extends Pokemon {
 
     public Salameche(String prenom) {
-        super(prenom, TypePokemon.FEU, 100, new Flameche());
+        super(prenom, "Salameche", TypePokemon.FEU, 100, new Flameche());
     }
 }
