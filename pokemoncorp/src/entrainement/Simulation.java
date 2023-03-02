6e87cd4853d12a06bb1c8933d74817629345547b
thau-emile <thau.emile@ntymail.com>
2023-03-02 09:46:42 +0100

    Init Simulation

---
+++ pokemoncorp/src/entrainement/Simulation.java
@@ -0,0 +1,29 @@
+package pokemoncorp.src.entrainement;
+
+public class Simulation {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void displayAllPokemon() {
+
+    }
+
+    private static void displayAllPokemonSortedByXP() {
+
+    }
+
+    private static void displayPokemonByID() {
+
+    }
+
+    private static void displayAllArene() {
+
+    }
+
+    private static void lancerUnCombat() {
+
+    }
+
+}
