2f2a41059d192d6106d52c41a0f9f2da336a1e2d
Nathalie CHARLES <charlnathalie@gmail.com>
2023-03-02 10:55:14 +0100

    wip arene filles

--- pokemoncorp/src/entrainement/MareAcide.java
+++ pokemoncorp/src/entrainement/MareAcide.java
@@ -2,15 +2,19 @@ package pokemoncorp.src.entrainement;
 
 import pokemoncorp.src.referentiel.Pokemon;
 
-public abstract class MareAcide extends Arene {
+public class MareAcide extends Arene {
     
     public MareAcide(){
-
+        super("MareAcide");
     };
 
     @Override
     public void effetTourDeCombat(Pokemon pokemon1, Pokemon pokemon2){
         //perte de 5pts de vie
+    }
+
+    @Override
+    public void effetDebutCombat(Pokemon pkA, Pokemon pkB) {
     };
 
 }
--- pokemoncorp/src/entrainement/Volcan.java
+++ pokemoncorp/src/entrainement/Volcan.java
@@ -2,14 +2,18 @@ package pokemoncorp.src.entrainement;
 
 import pokemoncorp.src.referentiel.Pokemon;
 
-public abstract class Volcan extends Arene {
+public class Volcan extends Arene {
 
     public Volcan(){
-
+        super("Volcan");
     };
 
     @Override
     public void effetDebutCombat(Pokemon pokemon1, Pokemon pokemon2){
         //perte de 20pts de vie
+    }
+
+    @Override
+    public void effetTourDeCombat(Pokemon pkA, Pokemon pkB) {
     };
 }
